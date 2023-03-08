e2c1e2d9ca9afbf30e79b67c83fc100718e6f6b0
The_ai <sans34678@gmail.com>
2023-03-08 00:02:17 -0500

    A lot of progress has been made Added tabs Resource window Settings Beginning of the story

--- mainscript.js
+++ mainscript.js
@@ -1,14 +1,58 @@
-
+var resources = new Map();
 
 
 
 function generateMain() {
-    const test = document.getElementById("test");
-    test.textContent = " ";
-    //localStorage.setItem("test", "1");
-    alert(localStorage.getItem("test"));
+    newGame();
 }
 
-function gamescript() {
+function newGame() {
+    initNewGameVar();
+    displayResources(resources);
+    showStory();
+    addLog("A puppy leaves the noisy and polluted city and sets up a camp away from the city for puppies to go. First they must scavange for food.")
+}
 
+function initNewGameVar() {
+    resources.set("dogFood", 0);
+}
+
+function displayResources() {
+    document.getElementById("dogFoodDisplay").textContent = resources.get("dogFood");
+}
+
+function gameTick() {
+
+}
+
+function showCamp() {
+    hideWindows();
+    document.getElementById("campWindow").hidden = false;
+}
+
+function showSettings() {
+    hideWindows();
+    document.getElementById("settingsWindow").hidden = false;
+}
+
+function showStory() {
+    hideWindows();
+    document.getElementById("storyWindow").hidden = false;
+}
+
+function hideWindows() {
+    document.getElementById("campWindow").hidden = true;
+    document.getElementById("settingsWindow").hidden = true;
+    document.getElementById("storyWindow").hidden = true;
+}
+
+function resetGame() {
+    if (confirm("Are you sure you want to COMPLETELY reset all progress?")) {
+        newGame();
+    }
+}
+
+function addLog(text) {
+    const storyBox = document.getElementById("storyBox");
+    storyBox.textContent = storyBox.textContent + "> " + text +  " \n";
 }
