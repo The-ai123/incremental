2de2b904b9ec738cea396bbc0a59d4dfd320b6c1
The_ai <sans34678@gmail.com>
2023-03-08 21:59:07 -0500

    Added documentation

--- mainscript.js
+++ mainscript.js
@@ -3,10 +3,12 @@ var jobs = new Map();
 var elements = new Map();
 var production = new Map();
 
+//On load function
 function generateMain() {
     newGame();
 }
 
+//Starts a new game
 function newGame() {
     initNewGameVar();
     displayResources(resources);
@@ -14,6 +16,7 @@ function newGame() {
     addLog("A puppy leaves the noisy and polluted city and sets up a camp away from the city for puppies to go. First they must scavenge for food.")
 }
 
+//reset variables
 function initNewGameVar() {
     elements = new Map([
         ["campWindow", document.getElementById("campWindow")],
@@ -28,6 +31,7 @@ function initNewGameVar() {
     jobs.set("unemployed", 1);
 }
 
+//update resource display
 function displayResources() {
     document.getElementById("dogFoodDisplay").textContent = resources.get("dogFood");
     document.getElementById("puppiesDisplay").textContent = jobs.get("unemployed") + "/" + resources.get("puppies");
@@ -37,28 +41,31 @@ function gameTick() {
 
 }
 
-
+//show camp tab
 function showCamp() {
-
     hideWindows();
     elements.get("campWindow").hidden = false;
 }
 
+//show settings tab
 function showSettings() {
     hideWindows();
     elements.get("settingsWindow").hidden = false;
 }
 
+//show story tab
 function showStory() {
     hideWindows();
     elements.get("storyWindow").hidden = false;
 }
 
+//show city tab
 function showCity() {
     hideWindows();
     elements.get("cityWindow").hidden = false;
 }
 
+//hide all windows
 function hideWindows() {
     elements.get("campWindow").hidden = true;
     elements.get("settingsWindow").hidden = true;
@@ -66,30 +73,37 @@ function hideWindows() {
     elements.get("cityWindow").hidden = true;
 }
 
+//reset game
 function resetGame() {
     if (confirm("Are you sure you want to COMPLETELY reset all progress?")) {
         newGame();
     }
 }
 
+//change a specified resource by a certain amount
 function changeResource(name, amount) {
     resources.set(name, resources.get(name) + amount);
+    //give a warning if a resource goes below 0, this probably shouldn't happen
     if (resources.get(name) < 0) {
         console.log("Warning: Resource '" + name + "' is negative");
     }
 }
 
+//change a specified job by a certain amount
 function changeJobs(name, amount) {
     jobs.set(name, jobs.get(name) + amount);
+    //give a warning if a resource goes below 0, this probably shouldn't happen
     if (jobs.get(name) < 0) {
         console.log("Warning: Job '" + name + "' is negative");
     }
 }
 
+//Write text in the story box
 function addLog(text) {
-    elements("storyBox").textContent = elements("storyBox").textContent + "> " + text +  " \n";
+    elements.get("storyBox").textContent = elements.get("storyBox").textContent + "> " + text +  " \n";
 }
 
+//change the amount of people scavenging
 function changeCityScavenge(amount) {
     if (jobs.get("cityScavenge") >= -1*amount && jobs.get("unemployed") >= amount){
         changeJobs("cityScavenge", amount);
